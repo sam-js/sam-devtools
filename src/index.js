9c2ad45accdfa433a5b40ed3cea6580083f9287b
Gunar C. Gessner <gunar@gunargessner.com>
2016-03-05 17:31:13 -0300

    Prevent infinite nap loop. Fixes #3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-// import { createModel } from 'sam'
-
+import deepEqual from 'deep-equal'
 /*
  * This is a dummy function to check if the function name has been altered by minification.
  * If the function has been minified and NODE_ENV !== 'production', warn the user.
@@ -15,6 +14,7 @@ if (
   console.log('You are currently using minified code outside of NODE_ENV === \'production\'.')
 }
 
+const MAX_PRESENTS_FROM_NAP = 10
 let snapshots = []
 let model
 const listeners = []
@@ -36,6 +36,16 @@ const publish = _ => listeners.forEach(listener => listener(snapshots, loadSnaps
 
 export function liftContainer(container) {
   return (store, dataset) => {
+    // check for infinite loop in nap
+    if (dataset['@@napLoop']) {
+      const newStore = { '@napLoop': true }
+      snapshots.push({
+        dataset: { ...dataset },
+        store: {},
+      })
+      publish()
+      return newStore
+    }
     const newStore = container(store, dataset)
     snapshots.push({
       dataset: { ...dataset },
@@ -52,15 +62,34 @@ const getSnapshot = index => {
   return { ...snapshots[index].store }
 }
 
+const fromNap = snapshot => snapshot.dataset['@@nap'] !== undefined
+
 export function liftNap(nap) {
   return currentState => {
     return present => {
       return nap(currentState)(dataset => {
+        // check for infinite loop in nap
+        const lastSnapshot = snapshots[snapshots.length -1]
+
+        if (lastSnapshot.dataset['@@napLoop']) {
+          return
+        }
+
+        const tooManyPresentsFromNAP = snapshots.slice(-MAX_PRESENTS_FROM_NAP).filter(fromNap).length == MAX_PRESENTS_FROM_NAP
+        if (tooManyPresentsFromNAP) {
+          return present({ '@@napLoop': 'too many present()s from NAP' })
+        }
+
+        const secondLastSnapshot = snapshots[snapshots.length -2]
+        const ineffectiveNAP = fromNap(lastSnapshot) && fromNap(secondLastSnapshot) && deepEqual(lastSnapshot.store, secondLastSnapshot.store)
+        if (ineffectiveNAP) {
+          return present({ '@@napLoop': 'last present() from NAP didn\'t change the store' })
+        }
         const liftedDataset = {
           ...dataset,
           '@@nap': true,
         }
-        return present(liftedDataset)
+        return(present(liftedDataset))
       })
     }
   }
